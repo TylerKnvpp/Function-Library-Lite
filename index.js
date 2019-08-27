7ec0dbcc55b698f887a6979af89cec92378f2352
Tyler Knapp <tgknapp11@gmail.com>
2019-08-27 10:03:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Insert code here!!
+let array = [1, 2, 3, 4, 5, 6];
+
+// EACH
+function myEach(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    callback(element);
+  }
+}
+
+function square(e) {
+  return e * e;
+}
+
+function log(e) {
+  console.log(e);
+}
+
+function even(e) {
+  if (e % 2 == 0) {
+    return true;
+  }
+}
+
+function greaterThan(e) {
+  if (e > 3) {
+    return true;
+  }
+}
+
+// MAP
+function myMap(arr, callback) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    newArray.push(callback(element));
+  }
+  return newArray;
+}
+
+// FILTER
+function myFilter(arr, callback) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+
+    if (callback(element) == true) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
+}
+
+// FIND
+function myFind(arr, callback) {
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (callback(element) == true) return element;
+  }
+}
